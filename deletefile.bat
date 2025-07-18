edac4608f20dd3dbf2161486f757e1f000cb9106
ambujshukla04 <2k22.csds.2211189@gmail.com>
2025-07-18 16:33:33 +0530

    bot added

---
+++ deletefile.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+:: Set the path where all profiles are stored
+set "PROFILE_DIR=C:\Users\Lenovo\selenium_profiles"
+
+echo [🧹] Cleaning up temporary profiles in: %PROFILE_DIR%
+
+:: Loop through all folders in the profile directory
+for /d %%F in ("%PROFILE_DIR%\*") do (
+    if /I not "%%~nxF"=="instagram_profile" (
+        echo Deleting folder: %%F
+        rmdir /s /q "%%F"
+    )
+)
+
+echo [✔] Cleanup complete. Only 'instagram_profile' retained.
+pause
